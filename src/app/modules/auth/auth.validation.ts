dd7641decee4732293184f3859fcbbfbb5f1f683
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-05-23 21:59:46 +0600

    up to date

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,16 +1,11 @@
 import { z } from "zod";
 
 export const userValidationSchema = z.object({
-  name: z
-    .string()
-    .min(2, "Name must be at least 2 characters long"),
+  name: z.string().min(2, "Name must be at least 2 characters long"),
 
-  email: z
-    .email("Invalid email address"),
+  email: z.email("Invalid email address"),
 
-  password: z
-    .string()
-    .min(6, "Password must be at least 6 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
 
   age: z
     .number()
@@ -20,14 +15,10 @@ export const userValidationSchema = z.object({
 });
 
 export const loginValidationSchema = z.object({
-    email: z
-      .email("Invalid Email Address"),
+  email: z.email("Invalid Email Address"),
 
-    password:z
-      .string()
-      .min(6, "Password must be at least 6 characters")  
-
-})
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
 
 export type UserRegister = z.infer<typeof userValidationSchema>;
 export type UserLogin = z.infer<typeof loginValidationSchema>;
